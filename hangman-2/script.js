381293232f894194f7d1da95eca934ff046011d5
David Kurth <david-kurth@sbcglobal.net>
2017-02-15 16:18:00 -0600

    adding hangman part 2 starting point

---
+++ hangman-2/script.js
@@ -0,0 +1,19 @@
+.title {
+  font-size: 24px;
+  font-family: 'Droid Serif', serif;
+  font-weight: bold;
+  padding-top: 20px;
+  text-align: center;
+}
+
+header {
+  width: 100%;
+  background-color: lightgray;
+  height: 40px;
+}
+header ul li {
+  list-style: none;
+  float: right;
+  padding: 10px;
+  font-weight: bold;
+}
