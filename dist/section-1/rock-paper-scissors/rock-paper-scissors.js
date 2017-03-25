fabb33eec167ad694802f4394364982ebedb15fa
David Kurth <david-kurth@sbcglobal.net>
2017-03-25 08:31:52 -0500

    adding rock vs

--- dist/section-1/rock-paper-scissors/rock-paper-scissors.js
+++ dist/section-1/rock-paper-scissors/rock-paper-scissors.js
@@ -3,4 +3,7 @@ function whoWins(first, second) {
   if(first === 'rock' && second === 'scissors') {
     return 'first';
   }
+  if(first === 'scissors' && second === 'rock') {
+    return 'second';
+  }
 }
