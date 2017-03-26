4f2a3758127377c5eb69b9ffd99b5b946ec7d05c
David Kurth <david-kurth@sbcglobal.net>
2017-03-26 16:02:27 -0500

    rock paper scissor done

--- dist/section-1/rock-paper-scissors/rock-paper-scissors.js
+++ dist/section-1/rock-paper-scissors/rock-paper-scissors.js
@@ -6,4 +6,25 @@ function whoWins(first, second) {
   if(first === 'scissors' && second === 'rock') {
     return 'second';
   }
+  if(first === 'paper' && second === 'rock') {
+    return 'first';
+  }
+  if(first === 'rock' && second === 'paper') {
+    return 'second';
+  }
+  if(first === 'scissors' && second === 'paper') {
+    return 'first';
+  }
+  if(first === 'paper' && second === 'scissors') {
+    return 'second';
+  }
+  if(first === 'paper' && second === 'paper') {
+    return 'tie';
+  }
+  if(first === 'scissors' && second === 'scissors') {
+    return 'tie';
+  }
+  if(first === 'rock' && second === 'rock') {
+    return 'tie';
+  }
 }
