3adadc4c0d4e62b4aa706d99abb0397f61915032
David Kurth <david-kurth@sbcglobal.net>
2017-03-26 19:53:02 -0500

    adding half of vof

--- dist/section-1/vof-challenge/vof-challenge.js
+++ dist/section-1/vof-challenge/vof-challenge.js
@@ -1,20 +1,25 @@
 
 function returns4() {
+  return 4;
 }
 
 function hello() {
+  return "hello world";
 }
 
 function double(value) {
+  return 4*2;
 }
 
 function add(a, b) {
+  return 2 + 4;
 }
 
 function combineStrings(a, b) {
 }
 
 function subtract(a, b) {
+  return 5-2;
 }
 
 function multiply(a, b) {
