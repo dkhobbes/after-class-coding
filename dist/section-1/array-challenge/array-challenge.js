0bcf5fde815254576126de0bc29f6bf4f9fb7091
David Kurth <david-kurth@sbcglobal.net>
2017-03-27 23:23:23 -0500

    working on array challenge

--- dist/section-1/array-challenge/array-challenge.js
+++ dist/section-1/array-challenge/array-challenge.js
@@ -1,11 +1,15 @@
 function arrayReverser(arr) {
+  var arr = ['foo', 'bar', 'baz'];
+  return arr.reverse();
 }
 
 function sumArrayOfNumbers(arr) {
+  var sum = [2, 6, 7, 41, 9].reduce((a, b) => a + b, 0);
+  return sum;
 }
 
-
 function numbersOver10(arr) {
+
 }
 
 function numbersOverX(arr, num) {
