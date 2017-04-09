4f8e6c25e58f3a260efbc2be4f93cbad8e3c8bcc
David Kurth <david-kurth@sbcglobal.net>
2017-04-09 14:29:16 -0500

    adding basic toggle on class in react

--- react-webpack-practice/client/js/header.jsx
+++ react-webpack-practice/client/js/header.jsx
@@ -2,11 +2,33 @@ import React from 'react';
 
 class Header extends React.Component {
 
+constructor() {
+  super();
+  console.log('firing constructor');
+
+  this.state= {
+    currentClass:'start-state'
+  };
+}
+
+toggle(){
+  if(this.state.currentClass === 'on-state'){
+    this.setState({
+      currentClass: 'start-state'
+    });
+  }
+  else {
+    this.setState({
+      currentClass: 'on-state'
+    });
+  }
+}
+
   render () {
     return (
       <header>
         <h1>Welcome Header!</h1>
-        <div className="toggle">toggle</div>
+        <div className={this.state.currentClass} onClick={() => {this.toggle(); }}>Click this to toggle in React</div>
       </header>
     );
   }
--- react-webpack-practice/public/app.bundle.js
+++ react-webpack-practice/public/app.bundle.js
@@ -21552,7 +21552,7 @@
 /* 178 */
 /***/ function(module, exports, __webpack_require__) {
 
-	"use strict";
+	'use strict';
 	
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 	
@@ -21574,24 +21574,48 @@
 	  function Header() {
 	    _classCallCheck(this, Header);
 	
-	    return _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).apply(this, arguments));
+	    var _this = _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).call(this));
+	
+	    console.log('firing constructor');
+	
+	    _this.state = {
+	      currentClass: 'start-state'
+	    };
+	    return _this;
 	  }
 	
 	  _createClass(Header, [{
-	    key: "render",
+	    key: 'toggle',
+	    value: function toggle() {
+	      if (this.state.currentClass === 'on-state') {
+	        this.setState({
+	          currentClass: 'start-state'
+	        });
+	      } else {
+	        this.setState({
+	          currentClass: 'on-state'
+	        });
+	      }
+	    }
+	  }, {
+	    key: 'render',
 	    value: function render() {
+	      var _this2 = this;
+	
 	      return _react2.default.createElement(
-	        "header",
+	        'header',
 	        null,
 	        _react2.default.createElement(
-	          "h1",
+	          'h1',
 	          null,
-	          "Welcome Header!"
+	          'Welcome Header!'
 	        ),
 	        _react2.default.createElement(
-	          "div",
-	          { className: "toggle" },
-	          "toggle"
+	          'div',
+	          { className: this.state.currentClass, onClick: function onClick() {
+	              _this2.toggle();
+	            } },
+	          'Click this to toggle in React'
 	        )
 	      );
 	    }
