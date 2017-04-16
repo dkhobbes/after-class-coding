1e6f35f83ac28856b835e4256c4e0dc107afb30c
David Kurth <david-kurth@sbcglobal.net>
2017-04-16 17:49:47 -0500

    save point

---
+++ js-recursive-palindrome/script.js
@@ -0,0 +1,6 @@
+
+function isPalindrome(string) {
+  if(string.length <= 1) return true; //1
+  if(string.charAt(0) != string.charAt(string.length - 1)) return false; //2
+  return isPalindrome( string.substr(1, string.length -2) ); //3
+ }
