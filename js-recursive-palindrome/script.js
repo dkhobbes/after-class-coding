480c8d3754348dfc0cf7d2c974931109ced31c75
David Kurth <david-kurth@sbcglobal.net>
2017-04-16 17:51:52 -0500

    adding test into console log

--- js-recursive-palindrome/script.js
+++ js-recursive-palindrome/script.js
@@ -4,3 +4,5 @@ function isPalindrome(string) {
   if(string.charAt(0) != string.charAt(string.length - 1)) return false; //2
   return isPalindrome( string.substr(1, string.length -2) ); //3
  }
+
+ console.log(isPalindrome('anna')); // true
