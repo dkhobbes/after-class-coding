e56edbe2bb8bf5148d11b48ec948f2bd86807cd7
David Kurth <david-kurth@sbcglobal.net>
2017-04-17 17:59:41 -0500

    testing return add in component

--- freelance-EMC/client/js/script.js
+++ freelance-EMC/client/js/script.js
@@ -3,7 +3,10 @@ var mountNode = document.querySelector('#react-root');
 class AppComponent extends React.Component {
   render() {
     return <div>
-      <header></header>
+      <header>
+        <div className="margin_1"></div>
+        <div className="margin_2"></div>
+      </header>
     </div>;
   }
 }
