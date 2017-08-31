687d00d36c52dbd1bb50bf669ee69e4279108ac3
David Kurth <david-kurth@sbcglobal.net>
2017-08-30 20:20:02 -0500

    adding to gulpfile

--- flexbox-testing/gulpfile.js
+++ flexbox-testing/gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var sass = require('gulp-sass');
+var coffee = require('gulp-coffee');
+var uglify = require('gulp-uglify'),
+var concat = require('gulp-concat');
 
 
 gulp.task('copy', function() {
@@ -9,4 +13,25 @@ gulp.task('copy', function() {
 
 gulp.task('log', function() {
   gutil.log('== My Log Task ==')
+});
+
+gulp.task('sass', function() {
+  gulp.src('styles/main.scss')
+  .pipe(sass({style: 'expanded'}))
+    .on('error', gutil.log)
+  .pipe(gulp.dest('assets'))
+});
+
+gulp.task('coffee', function() {
+  gulp.src('scripts/hello.coffee')
+  .pipe(coffee({bare: true})
+    .on('error', gutil.log))
+  .pipe(gulp.dest('scripts'))
+});
+
+gulp.task('js', function() {
+  gulp.src('scripts/*.js')
+  .pipe(uglify())
+  .pipe(concat('script.js'))
+  .pipe(gulp.dest('assets'))
 });
