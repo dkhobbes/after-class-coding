12ee088ea31e5c8a4a23c282847d7519a9d86235
David Kurth <david-kurth@sbcglobal.net>
2017-11-12 17:02:31 -0600

    addign MQ

--- css-flexbox/script.js
+++ css-flexbox/script.js
@@ -1,6 +1,6 @@
-import style from "style.css";
-
-element.innerHTML =
-  `<h1 class="${style.title}">
-     An example heading
-   </h1>`;
+// import style from "style.css";
+//
+// element.innerHTML =
+//   `<h1 class="${style.title}">
+//      An example heading
+//    </h1>`;
