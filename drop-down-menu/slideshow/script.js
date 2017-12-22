3901fcae71a545772e0a93a57bf4ba910edfd31f
David Kurth <david-kurth@sbcglobal.net>
2017-12-21 23:59:14 -0600

    working on js

--- drop-down-menu/slideshow/script.js
+++ drop-down-menu/slideshow/script.js
@@ -1,3 +1,16 @@
 (function () {
-  var counter = 0;
+  var counter = 0,
+
+    $items = document.querySelectorAll('.slideshow-figure'),
+    numItems = $items.length;
+
+    var showCurrent = function() {
+      var itemShown = Math.abs(counter%numItems);
+
+      [].forEach.call( $items, function(el){
+        el.classList.remove('show');
+      });
+
+      $items[itemShown].classList.add('show');
+    };
 })();
